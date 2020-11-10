30c65c77d919886f39264d2ce25921ed0dc3b483
yue.fan <yue.fan@thoughtworks.com>
2020-11-10 03:57:50 -0500

    solve practice 2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,6 @@
-const parseData
+const parseData = (input) => {
+    let {data, column} = input;
+    let [{...note1}, {...note2}] = data;
+    return null;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+    let [section1, section2] = sections;
+    if (section2.index > section1.index) {
+        const array = [section1.content, items.slice(section1.index, section2.index), section2.content, items.slice(section2.index, items.length)];
+        return [].concat(...array);
+    }
+}
 export { inject };
